5881c0d23973a4b7f5044eb2299dda12b4dd0f25
ctil <ctil@users.noreply.github.com>
2016-11-05 15:56:11 -0600

    Update terminal in place
    The uilive library is used for this functionality. This eliminates
    the need to constantly clear the terminal.

--- pomodoro.go
+++ pomodoro.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gosuri/uilive"
 	"math"
 	"os"
 	"os/signal"
@@ -19,58 +20,56 @@ const (
 	lenProgressBar int = 78
 )
 
-func clearTerminal() {
-	fmt.Printf("\033[2J")
-	fmt.Printf("\033[H")
-}
-
-func updateDisplay(remainingTime time.Duration, totalTime time.Duration, iterationNum int,
-	name string) {
+func updateDisplay(writer *uilive.Writer, remainingTime time.Duration,
+	totalTime time.Duration, iterationNum int, name string) {
 	/* Updates the terminal with time remaining and a progress bar */
 	var minutes int = int(remainingTime.Minutes())
 	var seconds int = int(math.Floor(remainingTime.Seconds()+0.5)) % 60
 	var percentComplete float64 = float64(totalTime-remainingTime) / float64(totalTime)
 	var progress int = int(percentComplete * float64(lenProgressBar))
-	clearTerminal()
-	fmt.Printf("%s %d\n", name, iterationNum)
-	fmt.Printf("%v:%v (%v%%)\n", minutes, seconds, math.Floor(percentComplete*100))
-	fmt.Printf("|%s%s|\n", strings.Repeat("-", progress),
+	fmt.Fprintf(writer, "%s %d\n", name, iterationNum)
+	fmt.Fprintf(writer, "%v:%v (%v%%)\n", minutes, seconds, math.Floor(percentComplete*100))
+	fmt.Fprintf(writer, "|%s%s|\n", strings.Repeat("-", progress),
 		strings.Repeat(" ", lenProgressBar-progress))
+	writer.Flush()
 }
 
-func doIteration(duration time.Duration, iterationNum int, name string) {
+func doIteration(writer *uilive.Writer, duration time.Duration,
+	iterationNum int, name string) {
 	/* Runs a single iteration of a pomodoro or rest period. */
 	var startTime time.Time = time.Now()
 	ticker := time.NewTicker(time.Second)
-	updateDisplay(duration, duration, iterationNum, name)
+	updateDisplay(writer, duration, duration, iterationNum, name)
 	for {
 		<-ticker.C
-		updateDisplay(duration-time.Since(startTime), duration, iterationNum, name)
+		updateDisplay(writer, duration-time.Since(startTime), duration, iterationNum, name)
 		if time.Since(startTime) > duration {
 			ticker.Stop()
-			updateDisplay(duration-time.Since(startTime), duration, iterationNum, name)
+			updateDisplay(writer, duration-time.Since(startTime), duration, iterationNum, name)
 			return
 		}
 	}
 }
 
-func printTransition(message string) {
+func printTransition(writer *uilive.Writer, message string) {
 	/* Prints a transition message */
-	fmt.Printf("%s", message)
+	fmt.Fprintf(writer, "%s", message)
 	for i := 0; i <= 5; i++ {
 		time.Sleep(time.Second / 5)
-		fmt.Printf(".")
+		fmt.Fprintf(writer, ".")
 	}
+	fmt.Fprintf(writer, "\n")
+	writer.Flush()
 }
 
-func printSummary(finishedPomodoros int, startTime time.Time) {
+func printSummary(writer *uilive.Writer, finishedPomodoros int, startTime time.Time) {
 	/* Prints a summary of the work completed */
 	if finishedPomodoros == 1 {
-		fmt.Println("\nFinished 1 Pomodoro!")
+		fmt.Fprintf(writer, "\nFinished 1 Pomodoro!\n")
 	} else {
-		fmt.Printf("\nFinished %d Pomodoros!\n", finishedPomodoros)
+		fmt.Fprintf(writer, "\nFinished %d Pomodoros!\n", finishedPomodoros)
 	}
-	fmt.Printf("Elapsed Time: %v\n", time.Since(startTime))
+	fmt.Fprintf(writer, "Elapsed Time: %v\n", time.Since(startTime))
 }
 
 func main() {
@@ -79,6 +78,8 @@ func main() {
 	var iterations int = 0
 	var resting bool = false
 	var startTime time.Time = time.Now()
+	writer := uilive.New()
+	writer.Start()
 
 	// Cleanup on CTRL-C
 	sig := make(chan os.Signal, 1)
@@ -89,28 +90,30 @@ func main() {
 		if !resting {
 			finishedPomodoros = iterationNum - 1
 		}
-		printSummary(finishedPomodoros, startTime)
+		printSummary(writer, finishedPomodoros, startTime)
+		writer.Stop()
 		os.Exit(0)
 	}()
 
 	for {
 		// Pomodoro
-		doIteration(defaultPomodoro, iterationNum, "Pomodoro")
+		doIteration(writer, defaultPomodoro, iterationNum, "Pomodoro")
 		resting = true
-		printTransition(fmt.Sprintf("Pomodoro %d finished! Starting rest period",
+		printTransition(writer, fmt.Sprintf("Pomodoro %d finished! Starting rest period",
 			iterationNum))
 
 		// Rest Period
-		doIteration(defaultRest, iterationNum, "Rest Period")
+		doIteration(writer, defaultRest, iterationNum, "Rest Period")
 
 		if iterationNum != 0 && iterationNum == iterations {
-			printSummary(iterationNum, startTime)
+			printSummary(writer, iterationNum, startTime)
 			break
 		} else {
 			iterationNum += 1
 			resting = false
-			printTransition(fmt.Sprintf("Starting Pomodoro %d",
+			printTransition(writer, fmt.Sprintf("Starting Pomodoro %d",
 				iterationNum))
 		}
 	}
+	writer.Stop()
 }
